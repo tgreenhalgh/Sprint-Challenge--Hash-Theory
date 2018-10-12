baf1c52f84c36fd33291e273a4028393728c762c
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-10-12 09:27:31 -0700

    finixhes ex2

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,6 +1,40 @@
 def reconstruct_trip(tickets):
-  pass 
+    # build our hashtable
+    hashTable = {}
+    for key in tickets:
+        if (key[0] == None):
+            hashTable["start"] = key[1]
+        elif key[1] == None:
+            hashTable[key[0]] = "end"
+        else:
+            hashTable[key[0]] = key[1]
+
+    # start our trip list
+    trip = []
+    if check_valid(hashTable, "start"):
+        dest = hashTable["start"]
+        trip.append(dest)
+    else:
+        return []
+
+    while (check_valid(hashTable, dest) and dest != "end"):
+        dest = hashTable[dest]
+        # we done yet?
+        if (dest == "end"):
+            return trip
+        #  keep on keeping on
+        trip.append(dest)
+    # not a complete trip, so return empty list
+    return []
+
+
+def check_valid(hashtable, key):
+    try:
+        hashtable[key]
+        return True
+    except:
+        return False
+
 
 if __name__ == '__main__':
-  # You can write code here to test your implementation using the Python repl
-  pass
+    pass
